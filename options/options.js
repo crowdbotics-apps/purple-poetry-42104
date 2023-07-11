b1f9b1a1e80b082656e1f36f7e67b40a9d676774
Crowdbotics <team@crowdbotics.com>
2023-07-11 19:09:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-poetry-42104",
+  url: "https://purple-poetry-42104.botics.co",
+  api: "https://purple-poetry-42104.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
